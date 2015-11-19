0e9c2370bda77916661a5d79d047c503dae13651
usum <wlrua2@gmail.com>
2015-11-19 06:58:06 +0000

    Add message attributes and rake db:drop and migrate

--- db/migrate/20151115123125_create_messages.rb
+++ db/migrate/20151115123125_create_messages.rb
@@ -1,6 +1,10 @@
 class CreateMessages < ActiveRecord::Migration
   def change
     create_table :messages do |t|
+      t.integer :sender_id
+      t.integer :receiver_id
+      t.string :message
+      t.boolean :flags
 
       t.timestamps null: false
     end
