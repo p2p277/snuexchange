d8fb6860da42dbded0d8df6bb491595292708989
gaebal_gd <inverliebt@gmail.com>
2015-11-19 07:50:37 +0000

    save info

--- db/migrate/20151118151440_add_name_to_users.rb
+++ db/migrate/20151118151440_add_name_to_users.rb
@@ -1,7 +1,7 @@
 class AddNameToUsers < ActiveRecord::Migration
   def change
     add_column :users, :name, :string
-    add_column :users, :gender, :boolean
+    add_column :users, :gender, :string
     add_column :users, :language_learn, :string
     add_column :users, :language_speak, :string
     add_column :users, :brief, :string
