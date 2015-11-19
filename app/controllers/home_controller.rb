39ea503751cc991636952a2444da1e5536395829
shhan826 <shhan826@gmail.com>
2015-11-19 07:34:57 +0000

    Change filtering function

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,12 @@
 class HomeController < ApplicationController
 
 	def index
+		
+	end
+	
+	def filtering
+		@filtered = User.where("language_speak LIKE ? AND language_learn LIKE ? AND gender LIKE ?", params[:learn], params[:teach], params[:sex])
+		redirect_to('/home/language')
 	end
 
 end
