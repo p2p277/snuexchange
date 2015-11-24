1999cf8829699bdad7f20e4fef031bee97791f36
insu0616 <insu0616@gmail.com>
2015-11-24 00:55:14 +0000

    insoo4

--- app/models/usercomment.rb
+++ app/models/usercomment.rb
@@ -1,15 +1,4 @@
 class Usercomment < ActiveRecord::Base
     belongs_to :user
-    # 별점 평균 매소드
-      def avg
-            sum = 0
-            Usercomment.each do |u|
-              sum += u.score
-            end
-            if Usercomment.count == 0
-                0
-            else
-               sum.to_f / Usercomment.count
-            end
-      end
+    
 end
