a75820226b1351d5544221434a56fd2c98d6916e
usum <wlrua2@gmail.com>
2015-12-10 11:59:47 +0000

    Message Add Photo

--- db/schema.rb
+++ db/schema.rb
@@ -53,18 +53,18 @@ ActiveRecord::Schema.define(version: 20151126071651) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "",                    null: false
-    t.string   "encrypted_password",     default: "",                    null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,                     null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                                             null: false
-    t.datetime "updated_at",                                             null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
     t.string   "name"
     t.string   "gender"
     t.string   "language_learn"
@@ -76,7 +76,7 @@ ActiveRecord::Schema.define(version: 20151126071651) do
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
     t.string   "confirmation_token"
-    t.datetime "confirmed_at",           default: '2015-12-09 08:23:12'
+    t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
   end
